1ffe5fb52f74f0b777cdbb20b7f66885c31f8e59
Paul ROBERT <pauulrobert@gmail.com>
2023-08-28 08:37:35 +0200

    Update .alacritty.yml Update .config/nvim/.git/FETCH_HEAD Update .config/nvim/.git/index Update .config/nvim/.git/logs/HEAD Update .config/nvim/.git/logs/refs/heads/v2.0 Update .config/nvim/.git/refs/heads/v2.0 Update .config/nvim/lua/custom/plugins.lua Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/custom/plugins.lua
+++ private_dot_config/nvim/lua/custom/plugins.lua
@@ -50,6 +50,20 @@ local plugins = {
 
   {
     "nvim-treesitter/nvim-treesitter",
+    -- config = function()
+    --   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    --   parser_config.typst = {
+    --     install_info = {
+    --       url = "https://github.com/SeniorMars/tree-sitter-typst", -- local path or git repo
+    --       files = { "src/parser.c", "src/scanner.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    --       -- optional entries:
+    --       branch = "main", -- default branch in case of git repo if different from master
+    --       generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    --       requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    --     },
+    --     filetype = "typst", -- if filetype does not match the parser name
+    --   }
+    -- end,
     opts = {
       ensure_installed = {
         "vim",
@@ -58,6 +72,7 @@ local plugins = {
         "python",
         "c",
         "latex",
+        -- "typst",
       },
     },
   },
