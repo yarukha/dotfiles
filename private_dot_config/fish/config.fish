a17c2c27a9aebfb091dbec432c29f688837fd71d
yarukha <pauulrobert@gmail.com>
2024-01-05 17:04:54 +0800

    Update .config/fish/config.fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -98,12 +98,12 @@ function copy
 end
 
 ## Useful aliases
-# Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons' # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons' # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="exa -a | egrep '^\.'" # show only dotfiles
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons' # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | egrep '^\.'" # show only dotfiles
 alias ip="ip -color"
 
 # Replace some more things with better alternatives
