f44dd0c814f99cd2154bd9264386983920595f39
yarukha <pauulrobert@gmail.com>
2024-06-20 14:27:07 +0800

    Update .config/fish/config.fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -102,7 +102,7 @@ end
 
 ## Useful aliases
 # Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias ls='eza -l --color=always --group-directories-first --icons' # preferred listing
 alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
 alias ll='eza -l --color=always --group-directories-first --icons' # long format
 alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
