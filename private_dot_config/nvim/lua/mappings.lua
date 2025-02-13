43c317ff0b00abf21b9325657a216f7bb7dc07b8
Paul Robert <pauulrobert@gmail.com>
2025-02-13 18:44:45 +0100

    Update .config/nvim/lua/mappings.lua

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -5,7 +5,7 @@ require("nvchad.mappings")
 local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "ff", "<ESC>")
+map("i", "jj", "<ESC>")
 
 map("n", "<C-b>", "<cmd> NvimTreeToggle<CR>", { desc = "Toggle nvimtree" })
 map("n", "<leader>e", "<cmd> setlocal spell!<CR>", { desc = "Toggle spellcheck" })
