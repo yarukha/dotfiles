f9f2b9d5c73f4128952e0a209acba0f9d5be2f1b
Paul Robert <paul.robert@cea>
2025-03-17 20:21:17 +0100

    Update .config/nvim/lua/plugins.lua

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -1,4 +1,10 @@
+-- THESE SHOULD BE ORDERED BY IMPORTANCE, WITH THE ONES MORE LIKELY TO BE REMOVED ON TOP
+-- MAYBE EVEN MAKE A SEPARATE CONFIG FILE FOR PLUGINS WE NEVER CHANGE
+
 return {
+	-- coq proofs
+	{ "whonore/Coqtail" },
+	-- new surround actions, use ysiw ""
 	{
 		"kylechui/nvim-surround",
 		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
@@ -9,6 +15,7 @@ return {
 			})
 		end,
 	},
+	-- note taking akin to emacss org mode
 	{
 		"nvim-neorg/neorg",
 		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
@@ -20,21 +27,68 @@ return {
 			},
 		}),
 	},
-	{ "tlaplus-community/tlaplus-nvim-plugin", lazy = false },
+	-- translates ascii symbols into unicode
+	-- { "tlaplus-community/tlaplus-nvim-plugin", lazy = false },
+
+	-- enable transparency
 	{ "xiyaowong/transparent.nvim", lazy = false },
+
+	-- :Linediff to diff two blocks
+	"AndrewRadev/linediff.vim",
+
+	{ "nvim-lua/popup.nvim" },
+	{ "nvim-lua/plenary.nvim" },
 	{
-		"smoka7/hop.nvim",
-		version = "*",
-		opts = {
-			keys = "etovxqpdygfblzhckisuran",
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				"nvim-telescope/telescope-media-files.nvim",
+				"nvim-telescope/telescope-frecency.nvim",
+			},
 		},
 	},
+
+	{
+		"lervag/vimtex",
+		init = function()
+			-- Use init for configuration, don't use the more common "config".
+		end,
+	},
+	{ "typicode/bg.nvim", lazy = false },
+	{ "sindrets/diffview.nvim" },
+	-- Override plugin definition options
+	{
+		"nvim-tree/nvim-web-devicons",
+		opts = {
+			override_by_extension = {
+				["ml"] = {
+					icon = "🐫",
+					name = "ocaml",
+				},
+			},
+			override_by_filename = {
+				["dune"] = {
+					icon = "💩",
+					name = "dune",
+				},
+			},
+		},
+	},
+
+	-- BELOW SHOULD NOT BE CHANGED
+	--
+	--
+	--
+
+	-- lsp config
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			require("configs.lspconfig")
 		end,
 	},
+	-- formatting
 	{
 		"stevearc/conform.nvim",
 		config = function()
@@ -43,8 +97,8 @@ return {
 			})
 		end,
 	},
-	"AndrewRadev/linediff.vim",
 
+	-- treesitter config
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		lazy = false,
@@ -85,46 +139,6 @@ return {
 					show = {
 						git = true,
 					},
-				},
-			},
-		},
-	},
-
-	{ "nvim-lua/popup.nvim" },
-	{ "nvim-lua/plenary.nvim" },
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			{
-				"nvim-telescope/telescope-live-grep-args.nvim",
-				"nvim-telescope/telescope-media-files.nvim",
-				"nvim-telescope/telescope-frecency.nvim",
-			},
-		},
-	},
-
-	{
-		"lervag/vimtex",
-		init = function()
-			-- Use init for configuration, don't use the more common "config".
-		end,
-	},
-	{ "typicode/bg.nvim", lazy = false },
-	{ "sindrets/diffview.nvim" },
-	-- Override plugin definition options
-	{
-		"nvim-tree/nvim-web-devicons",
-		opts = {
-			override_by_extension = {
-				["ml"] = {
-					icon = "🐫",
-					name = "ocaml",
-				},
-			},
-			override_by_filename = {
-				["dune"] = {
-					icon = "💩",
-					name = "dune",
 				},
 			},
 		},
