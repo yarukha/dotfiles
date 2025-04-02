bf197c8b0c8922af57146290b466573d96e8002e
Paul Robert <paul.robert@cea>
2025-04-02 09:32:28 +0200

    Add .config/nvim/lua/plugins/utils.lua

---
+++ private_dot_config/nvim/lua/plugins/utils.lua
@@ -0,0 +1,51 @@
+return {
+	-- lazygit inside nvim
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+
+		-- lazygit inside nvim
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+
+	-- vim spreadsheet editing, use <leader>+sc
+	{ "mipmip/vim-scimark", event = { "BufReadPre *.norg", "BufNewFile *.norg" } },
+
+	-- note taking akin to emacss org mode
+	{
+		"nvim-neorg/neorg",
+		lazy = false,
+		version = "*",
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {},
+					["core.concealer"] = {},
+					["core.dirman"] = {
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+							default_workspace = "notes",
+						},
+					},
+				},
+			})
+		end,
+	},
+}
